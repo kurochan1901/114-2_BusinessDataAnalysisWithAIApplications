fb7dbd19ab9ceb56e86409f1a6a96fc6614c7f54
kurochan1901 <superstickman19.mc@gmail.com>
2026-04-20 14:19:40 +0800

    0416HW_chr()

---
+++ HW5_chr_function_111401546.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+for i in range(10):
+    for j in range(10):
+        if (j + i) % 2 == 0:
+            print(chr(65 + (j + i) % 10), end='')
+        else:
+            print(chr(97 + (j + i) % 10), end='')
+    print(end='\n')
+    
+for i in range(10): print(''.join(chr(65+(j+i)%10) if (j+i)%2 == 0 else chr(97+(j+i)%10) for j in range(10)))
+
+print(''.join(chr(65 + (j + i) % 10) if (j + i) % 2 == 0 else chr(97 + (j + i) % 10) for j in range(10)) for i in range(10))
+#why?
