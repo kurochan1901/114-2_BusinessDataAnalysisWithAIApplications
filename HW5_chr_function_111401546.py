a620719701a7a87346c38309aa6b647276130212
kurochan1901 <superstickman19.mc@gmail.com>
2026-04-21 09:23:49 +0800

    add HW5 explain

--- HW5_chr_function_111401546.py
+++ HW5_chr_function_111401546.py
@@ -12,3 +12,8 @@ for i in range(10): print(''.join(chr(65+(j+i)%10) if (j+i)%2 == 0 else chr(97+(
 
 print(''.join(chr(65 + (j + i) % 10) if (j + i) % 2 == 0 else chr(97 + (j + i) % 10) for j in range(10)) for i in range(10))
 #why?
+
+a = [''.join(chr(65 + (j + i) % 10) if (j + i) % 2 == 0 else chr(97 + (j + i) % 10) for j in range(10)) for i in range(10)]
+#join: 串接字串(比較方便)   
+#chr(65 + (j + i) % 10) if (j + i) % 2 == 0 else chr(97 + (j + i) % 10) for j in range(10)) => 這裡是個 generator expression，會產生一個 generator 物件
+#所以不能直接使用 join 來串接。需要先將 generator 物件轉換成 list 或 tuple 才能使用 join。
