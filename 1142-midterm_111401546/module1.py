9b5bd8707a9ac6c605c6053e3c19405674702600
kurochan1901 <superstickman19.mc@gmail.com>
2026-05-15 11:25:35 +0800

    crawler test-stock3231_202501

--- 1142-midterm_111401546/module1.py
+++ 1142-midterm_111401546/module1.py
@@ -17,14 +17,14 @@ def twse_crawler(year=2019, mm=5, dd=22, stockNo=2330):
     import urllib.request
 
     import json
-    '''    
+      
     #----------------------------------------------------
     # 測試憑證用url
     #----------------------------------------------------
     import ssl
     from multiprocessing import context
 
-    url = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20190522&stockNo=2330'
+    url = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20250122&stockNo=3231'
 
     context = ssl._create_unverified_context()
 
@@ -33,38 +33,44 @@ def twse_crawler(year=2019, mm=5, dd=22, stockNo=2330):
 
     print(stock)
     
-    '''
     
     #----------------------------------------------------
     # 1. 連線至台灣證交所 (TWSE)，擷取 個股資料
     #----------------------------------------------------
     '''
     url_twse = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date='
-    url_history = url_twse + str(year) + str(mm).zfill(2) + str(dd).zfill(2) + '&stockNo=' + str(stockNo)
+    url_history = (
+        url_twse 
+        + str(year) 
+        + str(mm).zfill(2) 
+        + str(dd).zfill(2) 
+        + '&stockNo='
+        + str(stockNo)
+    )
     print(url_history)
     webpage_history = urllib.request.urlopen(url_history)
     web_html = bs.BeautifulSoup(webpage_history, 'html.parser')
     print(web_html)
 
     stock = json.loads(web_html.text)  #  讀取 JSON 資料
-    stock
+    #stock
 
-    
+    '''
+  
 
     #----------------------------------------------------
     # 2. 轉換 個股資料成 data frame 資料格式
     #----------------------------------------------------
     stock_info = list(stock.values())  #  轉換成 list 格式
     stock_info
-    # stock_info[2]
-    # stock_info[3]
-    '''
+    # stock_info[2]     title: 108年05月 2330 台積電 各日成交資訊
+    # stock_info[3]     fields: ['日期', '成交股數', '成交金額', '開盤價', '最高價', '最低價', '收盤價', '漲跌價差', '成交筆數', '註記']
+    
 
-    '''
     import pandas as pd
     import numpy as np
 
-    stock_price = pd.DataFrame(stock_info[4])
+    stock_price = pd.DataFrame(stock_info[4])   #data: 個股每日成交資訊資料
     stock_price
 
     ##  將 data frame 資料加上 column name
@@ -84,13 +90,13 @@ def twse_crawler(year=2019, mm=5, dd=22, stockNo=2330):
     # < 輸出個股資料至 csv 檔案 >
     #----------------------------------------------------
     import os.path
-    mydir ='./'      #  檔案路徑
+    mydir ='./'      #  檔案路徑--目前執行程式所在的目錄
     #  建立檔案路徑 + 檔案名稱 stock.csv
     csv_file = os.path.join(mydir, "stock" + str(stockNo) + '_' + str(year) + str(mm).zfill(2) + ".csv")
     df_price.to_csv(csv_file, encoding='utf_8_sig')   #  輸出 csv 檔案 
     print(' 輸出 csv 檔案 : ', csv_file)
     return df_price  #   回傳 個股 data frame 資料
     
-'''
-twse_crawler()
+
+twse_crawler(year=2025, mm=1, dd=22, stockNo=3231)
 
